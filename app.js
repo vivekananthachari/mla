5d538afde97180d6417ed68fc9b472306c884697
vivek <vchari485@gmail.com>
2021-12-18 15:15:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -177,6 +177,6 @@ app.post('/api/upload-avatar', async (req, res) => {
 
 
 //Server listening
-app.listen(3000,() =>{
-  console.log('Server started on port 3000...');
+app.listen(5000,() =>{
+  console.log('Server started on port 5000...');
 });
